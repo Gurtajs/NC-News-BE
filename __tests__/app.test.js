de8d78f97282f9e8504e2b971b930f2a5fa0745e
Gurtaj <gurtajs98@gmail.com>
2024-04-19 22:57:47 +0100

    updated jest command

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -63,7 +63,7 @@ describe("/api/articles/:article_id", () => {
         expect(article.article_id).toBe(1);
         expect(article.body).toBe("I find this existence challenging");
         expect(article.topic).toBe("mitch");
-        expect(article.created_at).toBe("2020-07-09T20:11:00.000Z");
+        expect(article.created_at).toBe("2020-07-09T21:11:00.000Z");
         expect(article.votes).toBe(100);
         expect(article.article_img_url).toBe(
           "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
@@ -392,7 +392,7 @@ describe('/api/articles/:article_id', () => {
         topic: "mitch",
         author: "butter_bridge",
         body: "I find this existence challenging",
-        created_at: '2020-07-09T20:11:00.000Z',
+        created_at: '2020-07-09T21:11:00.000Z',
         votes: 100,
         article_img_url:
           "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
