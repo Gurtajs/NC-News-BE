ef1594b1a7206ac227b78a1e2399cb6f0069b2ea
Gurtaj <gurtajs98@gmail.com>
2024-04-19 11:56:43 +0100

    added yml file

--- app.js
+++ app.js
@@ -43,8 +43,9 @@ app.use((err, req, res, next) => {
   }
   if (err.status && err.message) {
     res.status(err.status).send({ message: err.message });
-  } else {
-    res.status(400).send({ message: "Bad request" });
+  } 
+  if (err.code === '22P02') {
+    res.status(400).send({ message: "Bad request"})
   }
 });
 
