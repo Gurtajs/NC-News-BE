eded42db221b07ecb00c312eaebaa8cb91166667
Gurtaj <gurtajs98@gmail.com>
2024-04-19 22:23:20 +0100

    changes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -55,13 +55,16 @@ describe("/api/articles/:article_id", () => {
       .get("/api/articles/1")
       .expect(200)
       .then(({ body }) => {
+        
         const article = body.article;
+        console.log(body)
         expect(article.author).toBe("butter_bridge");
         expect(article.title).toBe("Living in the shadow of a great man");
         expect(article.article_id).toBe(1);
         expect(article.body).toBe("I find this existence challenging");
         expect(article.topic).toBe("mitch");
-        expect(article.created_at).toBe("2020-07-09T20:11:00.000Z");
+        console.log(article.created_at)
+        expect(article.created_at).toBe("2020-07-09T21:11:00.000Z");
         expect(article.votes).toBe(100);
         expect(article.article_img_url).toBe(
           "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
